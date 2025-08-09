cf7fbf60b32fa1e365fd8b623a16bd30b8b0632e
Temi <limiajayi@gmail.com>
2025-08-09 16:43:17 +0100

    Building stuff.

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,4 +1,5 @@
 
+
 const Work = () => {
 
     return (
@@ -12,22 +13,7 @@ const About = () => {
 
     return (
         <div className="about">
-            <div className="rect aboutRect">
-                <div className="bar aboutBar">
-                    <h3>About</h3>
-                </div>
-
-                <div className="aboutContent">
-                    <img src="/me.jpg" alt="A picture of me" />
-                    <div className="aboutText">
-                        <h4>Temi Ajayi</h4>
-                        <p>
-                            Junior Developer interested in full stack development and AI !!!
-                        </p>
-                    </div>
-                </div>
-
-            </div>
+            
         </div>
     )
 }
@@ -35,8 +21,7 @@ const About = () => {
 const Info = () => {
     return (
         <div className="Info">
-            <About />
-            <p>HIII IM INFO</p>
+           
         </div>
     )
 }
