5282126ca2402da41aae6f37e04c61aa5a41db4e
Temi <limiajayi@gmail.com>
2025-08-11 11:08:05 +0100

    Making initial components for each section and separating them using grids.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,39 @@ import { useState } from 'react'
 import Nav from './components/Nav.jsx'
 import './styles/App.css'
 
+const Skills = () => {
+  return (
+    <div className="skills border">
+
+    </div>
+  )
+}
+
+const Projects = () => {
+  return (
+    <div className="projects border">
+
+    </div>
+  )
+}
+
+const Home = () => {
+  return (
+    <div className='home border'>
+
+    </div>
+  )
+}
+
 const App = () => {
   
   return (
     <>
     <Nav />
     <div className='main-page'>
-     
+      <Home />
+      <Projects />
+      <Skills />
     </div>
     </>
   )
