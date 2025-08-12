4d0515c3f743a15286a2349ef96c59ee7b20407c
Temi <limiajayi@gmail.com>
2025-08-12 08:55:42 +0100

    Importing particular repositories into the project section.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,50 @@
+import { useState, useEffect } from 'react'
 import './Projects.css'
 import Bar from './Bar'
 
-const Project = () => {
+const url = 'https://api.github.com/users/limiajayi/repos'
+
+const Project = ({ className, color, repo }) => {
   return (
-    <div className="project border">
-      <Bar text={"Project"} color={"purple"} />
-      hi im small project
+    <div className={className}>
+      <Bar text={"Project"} color={color} />
+      hi im a project
     </div>
   )
 }
 
 const Projects = () => {
+
+  const [repos, setRepos] = useState([])
+
+  const getRepos = async () => {
+    const response = await fetch(url)
+    const repos = await response.json();
+
+    const reposArray = repos.filter((repo) => {
+      return repo.name === "Actual-NEA" 
+      || repo.name === "Java-Space-Game" 
+      || repo.name === "motivation" 
+      || repo.name === "comprehension"
+    })
+
+    setRepos(reposArray)
+  }
+
+  useEffect(() => {
+    getRepos()
+  }, [])
+
+
+
   return (
     <div className="projects border">
-        
-        <div className="bigProject border">
-          <Bar text={"Big Project"} color={"pink"} />
-          Hi im big project
-        </div>
-        <Project />
-        <Project />
-        <Project />
+        <Project className={"bigProject border"} color={"pink"} />
+        <Project className={"project border"} color={"purple"} />
+        <Project className={"project border"} color={"purple"} />
+        <Project className={"project border"} color={"purple"} />
+
+        {console.log(repos)}
     </div>
   )
 }
