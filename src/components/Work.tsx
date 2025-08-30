23e5729201ce9a9e51d77ffa4de4c1bce5528a62
Temi <limiajayi@gmail.com>
2025-08-30 21:36:49 +0100

    Testing if state displays better with strings.

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -6,16 +6,15 @@ const fileName: string = 'workExperience.txt'
 
 const Work: FC = () => {
 
-    const [works, setWorks] = useState<string[]>([])
+    const [works, setWorks] = useState<string>("")
 
     const fetchTextFile = async (): Promise<void> => {
         try {
             //fetch the file full of my work experience
             const response: Response = await fetch(fileName)
             const text: string = await response.text()
-            const newArray: string[] = text.split(/\r\n\r\n/).filter(line => line.trim() !== '')
 
-            setWorks(newArray)
+            setWorks(text)
             
         } catch (error) {
             console.log("Error loading file: ", error)
@@ -24,12 +23,12 @@ const Work: FC = () => {
 
     useEffect(() => {fetchTextFile()}, [])
 
-    const arr: string[][] = works.map(line => line.split(/\r\n/))
+    const arr: string[] = works.split(/\r\n\r\n/)
 
     return (
         <div className="work border">
             <Bar text={"Work Experience"} color={"purple"} />
-            {arr.length > 0 && arr[0].length > 0 ? arr[0][0] : ""}
+            {arr.length > 0 && arr[0].length > 0 ? arr[0] : ""}
         </div>
     )
 }
