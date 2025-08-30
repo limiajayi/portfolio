e1f4be42ffbb1a3a6848564b5944afdd4e5c3952
Temi <limiajayi@gmail.com>
2025-08-30 19:28:25 +0100

    Got the file loader to work.

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState, type FC } from 'react'
 import '../styles/Work.css'
 import Bar from './Bar'
 
-const Work = () => {
+const fileName: string = 'workExperience.txt'
+
+const Work: FC = () => {
+
+    const [works, setWorks] = useState<string[]>([])
+
+    const fetchTextFile = async (): Promise<void> => {
+        try {
+            //fetch the file full of my work experience
+            const response: Response = await fetch(fileName)
+            const text: string = await response.text()
+            const newArray: string[] = text.split(/\r\n\r\n/).filter(line => line.trim() !== '')
+
+            setWorks(newArray)
+            
+        } catch (error) {
+            console.log("Error loading file: ", error)
+        }
+    }
+
+    useEffect(() => {fetchTextFile()}, [])
+
+    const arr: string[][] = works.map(line => line.split(/\r\n/))
+
     return (
         <div className="work border">
             <Bar text={"Work Experience"} color={"purple"} />
-            Work
+            {arr.length > 0 && arr[0].length > 0 ? arr[0][0] : null}
         </div>
     )
 }
