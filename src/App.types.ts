97749cc57f069c48b5703c59b1d4f8cfdadc4cfa
Temi <limiajayi@gmail.com>
2025-08-31 16:26:50 +0100

    Created project types.

--- src/App.types.ts
+++ src/App.types.ts
@@ -8,13 +8,27 @@ export type WorkExperienceProps =  {
     workLines: string[];
 }
 
+export type Repo = {
+    name: string;
+    description: string;
+    language: string;
+    clone_url: string;
+    homepage: string | undefined | null;
+}
+
+export type ProjectProps = {
+    text: string;
+    className: string;
+    color: string;
+    repo: Repo;
+}
+
 export type ProjectLinksProps = {
     url: string;
     homepage: string | undefined;
 }
 
 export type DescriptionProps = {
-    className: string;
     name: string;
     description: string;
     language: string;
