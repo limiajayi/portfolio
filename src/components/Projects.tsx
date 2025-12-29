5a95b836e4f92acead854f94b14047414b73af88
Temi <limiajayi@gmail.com>
2025-12-29 09:49:35 +0000

    Temporary project change.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,7 +18,7 @@ const Projects = () => {
             return repo.name === "Travelling-Agent" 
             || repo.name === "Java-Space-Game"
             || repo.name === "Affirmations"
-            || repo.name === "PlotThePlot-demo"
+            || repo.name === "NEA"
         })
 
         setRepos(reposArray)
